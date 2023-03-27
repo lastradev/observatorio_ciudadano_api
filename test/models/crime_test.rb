938c1b4dc24549ab14c0b5d9ecac41ec4401a6bc
Oscar Lastra <olastra@easybroker.com>
2023-03-26 21:57:58 -0600

    Add tests

--- test/models/crime_test.rb
+++ test/models/crime_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CrimeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should count total crimes in a year' do
+    count = Crime.where(year: 2020).year_count
+    assert_equal 24, count
+  end
 end
